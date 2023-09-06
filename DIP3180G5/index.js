7657b1124c68d33a2ccfb034a100f2376556690b
xiwencheong <xiwenmain@gmail.com>
2023-09-06 15:30:25 +0800

    login page initialization

--- DIP3180G5/index.js
+++ DIP3180G5/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import LoginScreen from './frontend/login page/login-screen';
+import LoginScreen from './frontend/login-page/login-screen';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
