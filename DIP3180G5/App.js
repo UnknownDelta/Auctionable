d6673f4649d1999d15bb83d3611d1cbba610504a
samantha <samanthacsea@gmail.com>
2023-09-06 15:59:15 +0800

    add loading page

--- DIP3180G5/App.js
+++ DIP3180G5/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoadingPage from './frontend/LoadingPage';
+import LoginPage from './frontend/login-screen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoadingPage">
+        <Stack.Screen name="LoadingPage" component={LoadingPage} options={{ headerShown: false }} />
+        <Stack.Screen name="LoginPage" component={LoginPage} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ DIP3180G5/frontend/LoadingPage.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { View, ImageBackground, Image, StyleSheet, Text } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+
+const backgroundImage = require("../assets/background.png");
+const logoImage = require("../assets/logo.png");
+
+export default function LoadingPage() {
+    const edges = useSafeAreaInsets();
+    const navigation = useNavigation();
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    // Function to load custom fonts
+    /*const loadFonts = async () => {
+        await Font.loadAsync({
+            "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+            "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+            "Roboto-LightItalic": require("../assets/fonts/Roboto-LightItalic.ttf"),
+            // Add more font families as needed
+        });
+        setFontsLoaded(true); // Set the state to indicate that fonts have been loaded
+    };*/
+
+    useEffect(() => {
+        //loadFonts();
+
+        // Use setTimeout to navigate to the homepage after 5 seconds
+        const timer = setTimeout(() => {
+            navigation.navigate("LoginPage"); // Replace "Home" with the name of your homepage screen
+        }, 5000); // 5000 milliseconds = 5 seconds
+
+        // Clean up the timer when the component unmounts
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+
+        <ImageBackground source={backgroundImage} style={styles.container}>
+
+            <View>
+                <Image source={logoImage} style={styles.logo} />
+            </View>
+            <View style={styles.loadingContent}>
+                <Text style={styles.loadingText}>Getting ready to drive deals {'\n'} your way, please wait...</Text>
+            </View>
+
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    logo: {
+        width: 199, // Adjust the width as needed
+        height: 100, // Adjust the height as needed
+    },
+    loadingContent: {
+        padding: 20,
+        borderRadius: 10,
+    },
+    loadingText: {
+        marginTop: 10,
+        fontSize: 17,
+        color: "#ffffff",
+        textAlign: 'center',
+        fontFamily: "Roboto-Regular",
+    },
+});
