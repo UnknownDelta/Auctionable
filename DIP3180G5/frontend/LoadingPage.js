6c9ffd2372264b02679f0640cbcbe3c474234d80
samantha <samanthacsea@gmail.com>
2023-09-08 00:46:10 +0800

    custom fonts to loadingpage

--- DIP3180G5/frontend/LoadingPage.js
+++ DIP3180G5/frontend/LoadingPage.js
@@ -3,52 +3,66 @@ import { View, ImageBackground, Image, StyleSheet, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import * as Font from "expo-font";
-import { useFonts } from "expo-font";
+import Apploading from "expo-app-loading";
+import { SplashScreen } from "expo-splash-screen"; // Import SplashScreen
 
 const backgroundImage = require("../assets/background.png");
 const logoImage = require("../assets/logo.png");
+const getFonts = () =>
+    Font.loadAsync({
+        "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+        "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+        "Roboto-LightItalic": require("../assets/fonts/Roboto-LightItalic.ttf"),
+    })
 
 export default function LoadingPage() {
     const edges = useSafeAreaInsets();
     const navigation = useNavigation();
     const [fontsLoaded, setFontsLoaded] = useState(false);
+    const [timeoutCompleted, setTimeoutCompleted] = useState(false);
 
-    // Function to load custom fonts
-    /*const loadFonts = async () => {
-        await Font.loadAsync({
-            "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
-            "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
-            "Roboto-LightItalic": require("../assets/fonts/Roboto-LightItalic.ttf"),
-            // Add more font families as needed
-        });
-        setFontsLoaded(true); // Set the state to indicate that fonts have been loaded
-    };*/
-
+    // Set a timeout to navigate to the login-screen after 5 seconds
     useEffect(() => {
-        //loadFonts();
+        const timeout = setTimeout(() => {
+            setTimeoutCompleted(true);
+        }, 5000);
 
-        // Use setTimeout to navigate to the homepage after 5 seconds
-        const timer = setTimeout(() => {
-            navigation.navigate("LoginPage"); // Replace "Home" with the name of your homepage screen
-        }, 5000); // 5000 milliseconds = 5 seconds
-
-        // Clean up the timer when the component unmounts
-        return () => clearTimeout(timer);
+        // Clear the timeout if it's no longer needed
+        return () => clearTimeout(timeout);
     }, []);
 
-    return (
+    // Check if the timeout has completed and navigate to the login-screen
+    useEffect(() => {
+        if (timeoutCompleted) {
+            navigation.navigate("LoginPage");
+        }
+    }, [timeoutCompleted, navigation]);
 
-        <ImageBackground source={backgroundImage} style={styles.container}>
 
-            <View>
-                <Image source={logoImage} style={styles.logo} />
-            </View>
-            <View style={styles.loadingContent}>
-                <Text style={styles.loadingText}>Getting ready to drive deals {'\n'} your way, please wait...</Text>
-            </View>
+    if (fontsLoaded) {
+        return (
+            <ImageBackground source={backgroundImage} style={styles.container}>
 
-        </ImageBackground>
-    );
+                <View>
+                    <Image source={logoImage} style={styles.logo} />
+                </View>
+                <View style={styles.loadingContent}>
+                    <Text style={styles.loadingText}>Getting ready to drive deals {'\n'} your way, please wait...</Text>
+                </View>
+
+            </ImageBackground>
+        );
+    } else {
+        return (
+            <Apploading
+                startAsync={getFonts}
+                onFinish={() => {
+                    setFontsLoaded(true);
+                }}
+                onError={console.warn}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
