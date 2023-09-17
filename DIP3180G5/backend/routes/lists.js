607f160cfd170301b047dca3a0f6adfc4bf1b2e9
UnknownDelta <36214939+UnknownDelta@users.noreply.github.com>
2023-09-17 22:25:25 +0800

    Version 1.3 User Table

--- DIP3180G5/backend/routes/lists.js
+++ DIP3180G5/backend/routes/lists.js
@@ -6,22 +6,28 @@ const {
     getSoldItems,
     updateItem
 } = require('../controllers/itemController')
+
+const {
+    getUser,
+    createUser
+} = require('../controllers/userController')
+
 const listing = express.Router()
 
 listing.get('/list', getItems)
 
 listing.get('/pastlist', getSoldItems)
 
-listing.get('/createlist', (req, res) =>{
-    res.json({mssg: 'placeholder page for creating list'})
-})
-
 listing.get('/updatelist', (req, res) =>{
     res.json({mssg: 'placeholder page for updating list'})
 })
 
-listing.post('/createlist/:id', createItem)
+listing.post('/createlist', createItem)
 
 listing.patch('/updatelist/:id', updateItem)
+
+listing.post('/createuser', createUser)
+
+listing.get('/user/:id', getUser)
 
 module.exports = listing
