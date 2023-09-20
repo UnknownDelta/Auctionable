3fe51cd71442327f53d678a731636ea8c228ad9e
UnknownDelta <36214939+UnknownDelta@users.noreply.github.com>
2023-09-20 14:31:52 +0800

    Version 1.45 Sending Images

--- DIP3180G5/backend/controller/itemController.js
+++ DIP3180G5/backend/controller/itemController.js
@@ -1,10 +1,17 @@
 const Items = require('../models/ItemsModel')
 const mongoose = require('mongoose')
+const multer = require('multer')
+
+const storage = multer.memoryStorage()
+const upload = multer({storage: storage})
+
+
 const getItems = async(req, res) =>{
     const itemList = await Items.find({}).sort({createdAt:-1})
 
     res.status(200).json(itemList)
 }
+
 //works partially, the case where there is 0 past items is glitchy
 const getSoldItems = async(req, res) =>{
     const soldItems = await Items.find({sold: true}).sort({createdAt:-1})
@@ -15,16 +22,17 @@ const getSoldItems = async(req, res) =>{
 
     res.status(200).json(soldItems)
 }
-const createItem = async(req, res) =>{
-    const {name, price, description, condition, years_used, category, new_used, images, seller, sold, qty} = req.body
+const createItem = (upload.single('image'), async (req, res) =>{
+    const {name, price, description, condition, years_used, category, new_used, seller, sold, qty} = req.body
 
     try {
+        const images = req.file.buffer.toString('base64')
         const item_list = await Items.create({name, price, description, condition, years_used, category, new_used, images, seller, sold, qty})
         res.status(200).json(item_list)
     } catch (error) {
         res.status(400).json({error: error.message})
     }
-}
+})
 
 const updateItem = async(req, res) =>{
     const {id} = req.params
