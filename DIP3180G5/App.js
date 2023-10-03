1da338660a4f9cb7ab00feb0472071e4d2893c15
xiwencheong <xiwenmain@gmail.com>
2023-10-03 21:43:17 +0800

    finished all functionalities of form creation

--- DIP3180G5/App.js
+++ DIP3180G5/App.js
@@ -4,20 +4,29 @@ import RegistrationScreen from './frontend/login-reg-page/registration-screen';
 import LoginScreen from './frontend/login-reg-page/login-screen';
 import ListingScreen from './frontend/listing-page/listing-screen';
 import CreateListingScreen from './frontend/listing-page/create-listing-screen';
-import CameraScreen from './frontend/listing-page/camera-screen';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'blue', // Change primary color to blue
+  },
+};
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen name="CameraScreen" component={CameraScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="ListingScreen" component={ListingScreen} options={{ headerShown: false}}/>
-        <Stack.Screen name="CreateListingScreen" component={CreateListingScreen} options={{ headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LoginScreen">
+          <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="ListingScreen" component={ListingScreen} options={{ headerShown: false}}/>
+          <Stack.Screen name="CreateListingScreen" component={CreateListingScreen} options={{ headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
