22c4caced39278fe419705071cb838e0b6e1fe4e
Bryan Noel Salindeho <bryannoelsalindeho23@gmail.com>
2023-11-16 20:58:46 +0800

    recover lost backend

---
+++ DIP3180G5/backend/routes/lists.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+const router = express.Router();
+// Import your car controller
+const carController = require('../controllers/carController')
+const userController = require('../controllers/userController')
+const auctionCarController = require('../controllers/auctionController')
+const transactionController = require('../controllers/transactionController')
+
+// car controller
+router.get('/', carController.getAllCars);
+router.get('/:id', carController.getCarDetails);
+router.get('/:seller/list', carController.getItems)
+router.get('/:seller/pastlist', carController.getSoldItems)
+router.post('/createlist', carController.createItem)
+router.patch('/updatelist/:id', carController.updateItem)
+
+// user controller
+router.get('/user/:id', userController.getUser)
+router.post('/createuser', userController.createUser)
+
+// auction controller
+router.get('/auctions', auctionCarController.getAllAuctionCars)
+router.get('/auctions/:id', auctionCarController.getAuction) // get a single auction car
+router.post('/createauction', auctionCarController.createAuction)
+router.get('/auctions/:seller', auctionCarController.getAuctionItems) // get a list of auction cars posted by a specific seller
+router.get('/pastauctions/:seller', auctionCarController.getAuctionSoldItems) // get a list of sold auctions cars by a specific seller
+router.patch('/updateauction/:id', auctionCarController.updateAuctionItem) // this is to update the current highest bidder
+
+// transaction controller
+router.post('/createtransaction', transactionController.createTransaction) // changed
+router.get('/:item_id/transaction', transactionController.getTransaction) // changed, everyone's transactions for a specific auction car (use in leaderboard)
+
+module.exports = router;
