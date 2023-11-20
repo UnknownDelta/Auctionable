e46e4147128551a87889b8cb3f6bf811dfe47359
Bryan Noel Salindeho <bryannoelsalindeho23@gmail.com>
2023-11-20 23:44:32 +0800

    auction controller

--- DIP3180G5/backend/controllers/auctionController.js
+++ DIP3180G5/backend/controllers/auctionController.js
@@ -20,11 +20,11 @@ const auctionCarController = {
         res.status(200).json(auctionList)
     },
     createAuction: async (req, res) =>{
-        const {brand, model, colour, fuel_type, mileage, buyout_price, starting_bid, ending_time, description, years_used, registration_date, category, new_used, images, seller, sold, reserve_price} = req.body
+        const {brand, model, buyout_price, starting_bid, reserve_price, ending_time, description, registration_date, images, seller_id, seller_name, seller_image, sold, highestBidder} = req.body
         try {
-            const auctionList = await Auction.create({brand, model, colour, fuel_type, mileage, buyout_price, starting_bid, ending_time, description, years_used, registration_date, category, new_used, images, seller, sold, reserve_price})
+            const auctionList = await Auction.create({brand, model, buyout_price, starting_bid, reserve_price, ending_time, description, registration_date, images, seller_id, seller_name, seller_image, sold, highestBidder})
             res.status(200).json(auctionList)
-            console.log(json(auctionList))
+            console.log(JSON.stringify(auctionList));
         } catch (error) {
             res.status(400).json({error: error.message})
             console.log(error.message)
